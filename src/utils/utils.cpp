db06ce5167dac5acdd89c375f249f35e6e1a7746
Michael Zhou <myz227@nyu.edu>
2019-07-25 21:39:05 -0400

    Modified debug helper function implementation

--- src/utils/utils.cpp
+++ src/utils/utils.cpp
@@ -189,7 +189,6 @@ bool load_rawfile(const std::string& rawfilename, const Eigen::RowVector3i& dims
     return true;
 }
 
-// void init_opengl_debug(void (*)(GLenum, GLenum, GLuint, GLenum, GLsizei, const GLchar *, const void *) callback) {
 void init_opengl_debug(GLDEBUGPROC callback) {
 #if !defined(__APPLE__)
     glEnable(GL_DEBUG_OUTPUT_SYNCHRONOUS);
